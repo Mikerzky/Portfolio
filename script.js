7a5f450738bb0ae7d546d417ff5fae8104b89f07
Mikerzky <mikerzkyy@gmail.com>
2022-11-16 09:11:04 +0700

    menambahkan file Index

---
+++ script.js
@@ -0,0 +1,56 @@
+// Navbar Responsive
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+
+function validasi()  {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if((username === "kelompok6")&& (password=="kelompok6")){
+        alert("selamat!, kamu berhasil login");
+        return true; 
+    }
+    else {
+        alert("Username & password kamu salah! coba lagi")
+    }
+}
+
+
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
